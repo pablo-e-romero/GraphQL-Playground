299febd817b72e22cc8830d7ab1bb6dcd32a4498
Pablo Ezequiel Romero <romero7116@gmail.com>
2022-01-10 18:51:10 +0100

    Initial Commit

---
+++ GraphQL-Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphQL-Playground
+//
+//  Created by Pablo Romero on 10/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphQL-Playground/GraphQL_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_PlaygroundApp.swift
+//  GraphQL-Playground
+//
+//  Created by Pablo Romero on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
